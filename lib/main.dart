73160c927fe3f391e27c6b281f5216f3d05812dd
Hyeongyu <admin@adminui-MacBookAir.local>
2023-12-17 08:36:26 +0900

    HK first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:cellpjt/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cellpjt/func1/login.dart';
 import 'package:cellpjt/bottomnav/mainfeed.dart';
 import 'package:cellpjt/bottomnav/grouplist.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  FirebaseOptions firebaseOptions = DefaultFirebaseOptions.currentPlatform;
+
+  // Firebase 앱을 초기화합니다.
+  await Firebase.initializeApp(
+    options: firebaseOptions,
+  );
+
   runApp(MyApp());
 }
 
